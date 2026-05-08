9b3180cf6c78e6137b5de039cddd5c53fa96b147
G-ibrahima <eb091941@ens.uqam.ca>
2026-05-08 18:56:58 -0400

    Ajout la classe CalculatriceConfig (@ConfigurationProperties @Component @Data)

---
+++ src/main/java/calculatrice/claculatriceREST/config/CalculatriceConfig.java
@@ -0,0 +1,18 @@
+package calculatrice.claculatriceREST.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "calculatrice")
+public class CalculatriceConfig {
+
+    private String message;
+    private String version;
+    private String nom;
+    private String auteur;
+
+}
--- src/main/java/calculatrice/claculatriceREST/controller/InfoController.java
+++ src/main/java/calculatrice/claculatriceREST/controller/InfoController.java
@@ -1,27 +1,22 @@
 package calculatrice.claculatriceREST.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import calculatrice.claculatriceREST.config.CalculatriceConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/calculator")
 public class InfoController {
 
-    @Value("${calculatrice.message}")
-    private String message;
-    @Value("${calculatrice.version}")
-    private String version;
+    private final CalculatriceConfig config;
+
+    public InfoController(CalculatriceConfig config) {
+        this.config = config;
+    }
 
     @GetMapping("/info")
-    public Map<String, String> getInfo() {
-        Map<String, String> result = new HashMap<>();
-        result.put("message", message);
-        result.put("version", version);
-        return result;
+    public CalculatriceConfig getInfo() {
+        return config;
     }
 }
